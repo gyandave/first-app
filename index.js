2c88985710eccfbb45c64533ee15c89f433b3c51
gyandave <gyanendradave@yahoo.com>
2019-02-17 17:12:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+// const EventEmitter = require('events');
+
+// const Logger = require('./logger');
+// const logger = new Logger();
+// //Ragister a listener
+// logger.on('messageLogged', (e) => {
+//   console.log('Listener Called', e)
+// })
+
+// logger.log('message');
+const _ = require('underscore');
+const mongoos = require('mongoose');
---
+++ logger.js
@@ -0,0 +1,16 @@
+// const EventEmitter = require('events');
+// // const emitter = new EventEmitter();
+
+// const url = 'http://mylooger.io/log';
+// class Logger extends EventEmitter {
+//     log(message) {
+//         console.log(message);
+    
+//         //Raise events
+//     this.emit('messageLogged', {id: 1, url: 'http://...'});
+//     }
+// }
+
+
+
+// module.exports = Logger;
